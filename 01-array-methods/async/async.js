85a8c31a23b9ba47bf3607f35fb77065aacfa271
antonzaratustra <antonzaratustra@gmail.com>
2023-07-26 14:33:48 +0300

    async await education

--- 01-array-methods/async/async.js
+++ 01-array-methods/async/async.js
@@ -1,3 +1,7 @@
+//Асинхронность работает в Event Loop
+
+//понятие Callback Hell
+
 // function delay(callback, time = 1000) {
 //     setTimeout(callback, time)
 // }
@@ -12,24 +16,87 @@
 
 //любую асинхронность мы можем поместить в promise
 
+// const delay = (time = 1000) => {
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, time)
+//     }) //называть константу можно как угодно  
+// return promise //можно выше сразу вернуть promise
+// }
+// //new Promise принимает аргументом функцию, в которую оборачиваем любой асинхронный код
+// //функция, которая попадает в конструктор new Promise 
+// //и в которую мы обернули асинхронный код
+// //принимает в себя два параметра resolve и reject
+// //resolve вызывается в async коде, чтобы сказать что он завершился
+// // и из функции delay возвращаем константу promise
+
+// delay(2500).then(() => {
+//     console.log('Timeout')
+// }) //данная функция возвращает нам promise, а у него есть методы
+//например then - после завершения promise
+
+//на promise сейчас пишется весь код, нужно поработать, разобраться     
+
 const delay = (time = 1000) => {
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve()
+            resolve([1, 2, 3])
         }, time)
-    }) //называть константу можно как угодно  
-return promise //можно выше сразу вернуть promise
+    })  
+return promise 
 }
-//new Promise принимает аргументом функцию, в которую оборачиваем любой асинхронный код
-//функция, которая попадает в конструктор new Promise 
-//и в которую мы обернули асинхронный код
-//принимает в себя два параметра resolve и reject
-//resolve вызывается в async коде, чтобы сказать что он завершился
-// и из функции delay возвращаем константу promise
 
-delay(2500).then(() => {
-    console.log('Timeout')
-}) //данная функция возвращает нам promise, а у него есть методы
-//например then - после завершения promise
+delay(2500) //к примеру здесь не искусственная задержка, а запрос на сервак
+    .then((data) => {
+        console.log('Timeout', data)
+         return data.map((x) => x ** 2)
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    .finally(() => console.log('Finally'))
 
-//на promise сейчас пишется весь код, нужно поработать, разобраться 
+//в параметр у then попадает результат из resolve
+//then может быть сколько угодно, это называется чейнинг
+//если сервер ответил ошибкой нам нужно как-то сообщить что data нет,
+//для этого вызываем reject('error in delay')
+//чтобы отловить ошибку есть метод catch
+//метод finally вызывается в любом случае
+
+
+const getData = () => new Promise(resolve => resolve([1, 2, 3]))
+//когда функция записана в одну строчку, то что после стрелки возвращается
+getData().then(array => console.log(array))
+//вызов getData возвращает промис
+
+//Есть другой синтаксис работы с асинхронностью async await
+//вместо then, catch, finally более нативный синтаксис try catch finally
+
+// function asyncExample() {
+//     try {
+        
+//     } catch (e) {
+
+//     } finally {
+
+//     }
+// }
+
+async function asyncExample() {
+        try {
+            await delay(3000) //дождись пока выполнится delay
+            const data = await getData() //работаем с промисом без then, понимаем что это проимс и испоьзуем await и добавляем async
+        } catch (err) {
+            console.log(err)
+        } finally {
+            console.log('Finally!')
+        }
+    }
+
+    //функция, которая работает с await должна быть async
+
+asyncExample()
